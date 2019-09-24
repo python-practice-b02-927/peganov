92f6299c9c219024a170cd3e5be18bcae8af8d98
PeganovAnton <peganoff2@mail.ru>
2019-09-24 14:01:53 +0300

    Add function for drawing background

--- lab3/pic9_1.py
+++ lab3/pic9_1.py
@@ -14,7 +14,13 @@ def draw_fishing_rod(win):
 
 
 def draw_background(win):
-    pass
+    """Draws sky and snow"""
+    sky = gr.Rectangle(gr.Point(-10, -10), gr.Point(610, 500))
+    sky.setFill('blue')
+    snow = gr.Rectangle(gr.Point(-10, 500), gr.Point(610, 1000))
+    snow.setFill('white')
+    sky.draw(win)
+    snow.draw(win)
 
 
 def draw_fish(win):
@@ -27,10 +33,10 @@ def draw_star(win):
 
 def main(win):
     """Draws picture"""
+    draw_background(win)
     draw_snowman(win)
     draw_ice_hole(win)
     draw_fishing_rod(win)
-    draw_background(win)
     draw_fish(win)
     draw_star(win)
 
