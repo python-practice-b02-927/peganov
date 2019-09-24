b93dd691a97936c84d5eb4606c6b130be2f01863
PeganovAnton <peganoff2@mail.ru>
2019-09-24 13:43:52 +0300

    Add main function definition

--- lab3/pic9_1.py
+++ lab3/pic9_1.py
@@ -1,9 +1,38 @@
 import graphics as gr
 
 
-def main(window):
-    """Draws picture"""
+def draw_snowman(win):
     pass
+
+
+def draw_ice_hole(win):
+    pass
+
+
+def draw_fishing_rod(win):
+    pass
+
+
+def draw_background(win):
+    pass
+
+
+def draw_fish(win):
+    pass
+
+
+def draw_star(win):
+    pass
+
+
+def main(win):
+    """Draws picture"""
+    draw_snowman(win)
+    draw_ice_hole(win)
+    draw_fishing_rod(win)
+    draw_background(win)
+    draw_fish(win)
+    draw_star(win)
 
 
 w = gr.GraphWin('pic9_1', 600, 1000)
