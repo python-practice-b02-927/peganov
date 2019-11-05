b3c204089ecc3969e95a51520be2a728291621f6
PeganovAnton <peganoff2@mail.ru>
2019-11-05 06:19:42 +0300

    Add saving mechanism

--- lab10/gun.py
+++ lab10/gun.py
@@ -1,7 +1,10 @@
+import json
 import math
+import os
 import tkinter as tk
 from abc import ABC, abstractmethod
 from random import randrange as rnd, choice
+from tkinter import filedialog
 
 import hit_check
 
@@ -21,18 +24,29 @@ class Agent(ABC):
         self.job = None
 
     @abstractmethod
-    def play(self):
-        pass
+    def start(self):
+        self.job = self.canvas.after(DT, self.update)
 
     @abstractmethod
-    def update(self):
-        pass
+    def play(self):
+        if self.job == 'pause':
+            self.job = self.canvas.after(DT, self.update)
 
     @abstractmethod
     def stop(self):
         if self.job is not None:
             self.canvas.after_cancel(self.job)
             self.job = None
+
+    @abstractmethod
+    def pause(self):
+        if self.job is not None and self.job != 'pause':
+            self.canvas.after_cancel(self.job)
+            self.job = 'pause'
+
+    @abstractmethod
+    def update(self):
+        pass
 
 
 class Ball(Agent):
@@ -58,8 +72,17 @@ class Ball(Agent):
         # цель.
         self.bullet_number = self.canvas.get_bullet_number()
 
+    def start(self):
+        super().start()
+
     def play(self):
-        self.job = self.canvas.after(DT, self.update)
+        super().play()
+
+    def stop(self):
+        super().stop()
+
+    def pause(self):
+        super().pause()
 
     def update(self):
         self.x += self.vx
@@ -74,9 +97,6 @@ class Ball(Agent):
                 self.job = self.canvas.after(DT, self.update)
             else:
                 self.destroy()
-
-    def stop(self):
-        super().stop()
 
     def destroy(self):
         self.stop()
@@ -107,6 +127,19 @@ class Ball(Agent):
                 t.destroy()
         return ids_hit
 
+    def get_state(self):
+        state = {
+            "x": self.x,
+            "y": self.y,
+            "vx": self.vx,
+            "vy": self.vy,
+            "r": self.r,
+            "color": self.color,
+            "live": self.live,
+            "job": self.job is not None
+        }
+        return state
+
 
 class Gun(Agent):
     def __init__(self, canvas):
@@ -131,19 +164,27 @@ class Gun(Agent):
 
         self.job = None
 
+    def start(self):
+        self.bind_all()
+        super().start()
+
     def play(self):
-        self.canvas.bind('<Button-1>', self.fire2_start, add='')
-        self.canvas.bind('<ButtonRelease-1>', self.fire2_end, add='')
+        if self.job == 'pause':
+            self.bind_all()
+        super().play()
 
-        root = self.canvas.get_root()
-        root.bind('<Up>', self.set_movement_direction_to_up, add='')
-        root.bind('<KeyRelease-Up>', self.stop_movement, add='')
-        root.bind('<Down>', self.set_movement_direction_to_down, add='')
-        root.bind('<KeyRelease-Down>', self.stop_movement, add='')
+    def stop(self):
+        super().stop()
+        self.unbind_all()
+        self.vy = 0
+        self.f2_power = 10
+        self.f2_on = 0
+        self.mouse_coords = [None, None]
 
-        self.mouse_coords = self.canvas.get_mouse_coords()
-
-        self.job = self.canvas.after(DT, self.update)
+    def pause(self):
+        super().pause()
+        self.unbind_all()
+        self.mouse_coords = [None, None]
 
     def update(self):
         self.gun_coords[1] += self.vy
@@ -155,18 +196,6 @@ class Gun(Agent):
             self.f2_power = self.min_gun_power
         self.redraw()
         self.job = self.canvas.after(DT, self.update)
-
-    def stop(self):
-        super().stop()
-        self.canvas.bind('<Button-1>', pass_event, add='')
-        self.canvas.bind('<ButtonRelease-1>', pass_event, add='')
-        root = self.canvas.get_root()
-        root.bind('<Up>', pass_event, add='')
-        root.bind('<KeyRelease-Up>', pass_event, add='')
-        root.bind('<Down>', pass_event, add='')
-        root.bind('<KeyRelease-Down>', pass_event, add='')
-        self.vy = 0
-        self.mouse_coords = [None, None]
 
     def update_angle(self):
         self.mouse_coords = self.canvas.get_mouse_coords()
@@ -202,7 +231,7 @@ class Gun(Agent):
         bullet_vx = self.f2_power * math.cos(self.an)
         bullet_vy = -self.f2_power * math.sin(self.an)
         bullet = Ball(self.canvas, *self.get_gunpoint(), bullet_vx, bullet_vy)
-        bullet.play()
+        bullet.start()
         self.f2_on = 0
         self.f2_power = 10
 
@@ -214,6 +243,37 @@ class Gun(Agent):
 
     def stop_movement(self, event):
         self.vy = 0
+
+    def bind_all(self):
+        self.canvas.bind('<Button-1>', self.fire2_start, add='')
+        self.canvas.bind('<ButtonRelease-1>', self.fire2_end, add='')
+
+        root = self.canvas.get_root()
+        root.bind('<Up>', self.set_movement_direction_to_up, add='')
+        root.bind('<KeyRelease-Up>', self.stop_movement, add='')
+        root.bind('<Down>', self.set_movement_direction_to_down, add='')
+        root.bind('<KeyRelease-Down>', self.stop_movement, add='')
+
+    def unbind_all(self):
+        self.canvas.bind('<Button-1>', pass_event, add='')
+        self.canvas.bind('<ButtonRelease-1>', pass_event, add='')
+        root = self.canvas.get_root()
+        root.bind('<Up>', pass_event, add='')
+        root.bind('<KeyRelease-Up>', pass_event, add='')
+        root.bind('<Down>', pass_event, add='')
+        root.bind('<KeyRelease-Down>', pass_event, add='')
+
+    def get_state(self):
+        state = {
+            "gun_coords": self.gun_coords,
+            "vy": self.vy,
+            "mouse_coords": self.mouse_coords,
+            "f2_power": self.f2_power,
+            "f2_on": self.f2_on,
+            "an": self.an,
+            "job": self.job is not None
+        }
+        return state
 
 
 class Target(Agent):
@@ -228,36 +288,35 @@ class Target(Agent):
         self.canvas = canvas
         self.id = self.canvas.create_oval(x-r, y-r, x+r, y+r, fill=color)
 
-    def play(self):
-        self.job = self.canvas.after(DT, self.update)
+    def start(self):
+        super().start()
 
-    def update(self):
-        self.job = self.canvas.after(DT, self.update)
+    def play(self):
+        super().play()
 
     def stop(self):
         super().stop()
 
+    def pause(self):
+        super().pause()
+
+    def update(self):
+        self.job = self.canvas.after(DT, self.update)
+
     def destroy(self):
-        print('destroy')
         self.stop()
         self.canvas.delete(self.id)
         del self.canvas.targets[self.id]
 
-
-class Menu(tk.Menu):
-    def __init__(self, master, game):
-        super().__init__(master)
-
-        self.game = game
-
-        self.file_menu = tk.Menu(self)
-        self.file_menu.add_command(label="save", command=self.master.save)
-        self.file_menu.add_command(label="load", command=self.master.load)
-        self.add_cascade(label="file", menu=self.file_menu)
-
-        self.game_menu = tk.Menu(self)
-        self.game_menu.add_command(label="new", command=self.master.new_game)
-        self.add_cascade(label="game", menu=self.game_menu)
+    def get_state(self):
+        state = {
+            "x": self.x,
+            "y": self.y,
+            "r": self.r,
+            "color": self.color,
+            "job": self.job is not None
+        }
+        return state
 
 
 class BattleField(tk.Canvas):
@@ -306,21 +365,35 @@ class BattleField(tk.Canvas):
             t = Target(self)
             self.targets[t.id] = t
 
+    def start(self):
+        self.catch_victory_job = self.after(DT, self.catch_victory)
+        self.gun.start()
+        for t in self.targets.values():
+            t.start()
+        for b in self.bullets.values():
+            b.start()
+
+    def play_jobs(self):
+        if self.catch_victory_job == 'pause':
+            self.catch_victory_job = self.after(DT, self.catch_victory)
+        if self.canvas_restart_job == 'pause':
+            self.canvas_restart_job = self.after(
+                VICTORY_MSG_TIME, self.restart)
+
     def play(self):
+        self.play_jobs()
         self.gun.play()
         for t in self.targets.values():
             t.play()
         for b in self.bullets.values():
             b.play()
 
-    def pause(self):
+    def stop(self):
         self.gun.stop()
         for t in self.targets.values():
             t.stop()
         for b in self.bullets.values():
             b.stop()
-
-    def cancel_jobs(self):
         if self.catch_victory_job is not None:
             self.after_cancel(self.catch_victory_job)
             self.catch_victory_job = None
@@ -328,16 +401,30 @@ class BattleField(tk.Canvas):
             self.after_cancel(self.canvas_restart_job)
             self.canvas_restart_job = None
 
+    def pause_jobs(self):
+        if self.catch_victory_job is not None:
+            self.after_cancel(self.catch_victory_job)
+            self.catch_victory_job = 'pause'
+        if self.canvas_restart_job is not None:
+            self.after_cancel(self.canvas_restart_job)
+            self.canvas_restart_job = 'pause'
+
+    def pause(self):
+        self.gun.pause()
+        for t in self.targets.values():
+            t.pause()
+        for b in self.bullets.values():
+            b.pause()
+        self.pause_jobs()
+
     def restart(self):
-        self.pause()
-        self.cancel_jobs()
+        self.stop()
         self.itemconfig(self.victory_text_id, text='')
         self.remove_targets()
         self.remove_bullets()
         self.create_targets()
         self.bullet_counter = 0
-        self.catch_victory_job = self.after(DT, self.catch_victory)
-        self.play()
+        self.start()
 
     def get_root(self):
         root = self.master
@@ -365,7 +452,7 @@ class BattleField(tk.Canvas):
         """
         if not self.targets:
             self.catch_victory_job = None
-            self.pause()
+            self.stop()
             self.show_victory_text()
         else:
             self.catch_victory_job = self.after(DT, self.catch_victory)
@@ -377,6 +464,19 @@ class BattleField(tk.Canvas):
     def report_hit(self, bullet, target):
         self.last_hit_bullet_number = bullet.bullet_number
         self.master.report_hit(bullet, target)
+
+    def get_state(self):
+        state = {
+            "gun": self.gun.get_state(),
+            "targets": [t.get_state() for t in self.targets.values()],
+            "bullets": [b.get_state() for b in self.bullets.values()],
+            "bullet_counter": self.bullet_counter,
+            "last_hit_bullet_number": self.last_hit_bullet_number,
+            "victory_text": self.itemcget(self.victory_text_id, 'text'),
+            "catch_victory_job": self.catch_victory_job is not None,
+            "canvas_restart_job": self.canvas_restart_job is not None
+        }
+        return state
 
 
 class MainFrame(tk.Frame):
@@ -404,11 +504,50 @@ class MainFrame(tk.Frame):
         self.score += 1
         self.score_label['text'] = self.score_tmpl.format(self.score)
 
+    def pause(self):
+        self.battlefield.pause()
+
+    def play(self):
+        self.battlefield.play()
+
+    def get_state(self):
+        state = {
+            'score': self.score,
+            'battlefield': self.battlefield.get_state()
+        }
+        return state
+
+
+class Menu(tk.Menu):
+    def __init__(self, master, game):
+        super().__init__(master)
+
+        self.game = game
+
+        self.file_menu = tk.Menu(self)
+        self.file_menu.add_command(label="save", command=self.master.save)
+        self.file_menu.add_command(label="load", command=self.master.load)
+        self.add_cascade(label="file", menu=self.file_menu)
+
+        self.game_menu = tk.Menu(self)
+        self.game_menu.add_command(label="new", command=self.master.new_game)
+        self.add_cascade(label="game", menu=self.game_menu)
+
 
 class GunGameApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry('{}x{}'.format(*WINDOW_SHAPE))
+
+        # Переменная `__file__` содержит путь к файлу, в котором используется.
+        #
+        # Функция `os.path.split()` разделяет путь к файлу на имя директории
+        # и имя файла.
+        #
+        # Функция `os.path.join()` объединяет несколько путей в один. Она
+        # самостоятельно подбирает разделитель в соответствии с операционной
+        # системой: '/' для UNIX и '\' для Windows.
+        self.save_dir = os.path.join(os.path.split(__file__)[0], 'save')
 
         self.main_frame = MainFrame(self.master)
         self.main_frame.pack(fill=tk.BOTH, expand=1)
@@ -416,14 +555,44 @@ class GunGameApp(tk.Tk):
         self.menu = Menu(self.master, self)
         self.config(menu=self.menu)
 
-    def save(self):
-        pass  # TODO
+        self.bind("<Control-s>", self.save)
+
+    def get_state(self):
+        return {'main_frame': self.main_frame.get_state()}
+
+    def get_save_file_name(self):
+        os.makedirs(self.save_dir, exist_ok=True)
+        file_name = filedialog.asksaveasfilename(
+            initialdir=self.save_dir,
+            title='Save game',
+            filetypes=(("json files", "*.json"), ("all files", "*.*"))
+        )
+        if file_name in [(), '']:
+            return None
+        return file_name
+
+    def save(self, event=None):
+        self.pause()
+        game_state = self.get_state()
+        file_name = self.get_save_file_name()
+        if file_name is not None:
+            with open(file_name, 'w') as f:
+                # Аргумент `indent` обеспечивает за красивое
+                # оформление JSON файла.
+                json.dump(game_state, f, indent=2)
+        self.play()
 
     def load(self):
         pass  # TODO
 
     def new_game(self):
         self.main_frame.new_game()
+
+    def pause(self):
+        self.main_frame.pause()
+
+    def play(self):
+        self.main_frame.play()
 
 
 app = GunGameApp()
